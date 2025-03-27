6243699719d61473b4073ee7091209748d9082e8
Leonardo Lessi <leonardolessi@gmail.com>
2025-03-27 20:40:21 -0300

    add crudTurma.py

--- Projeto Conclusao Semestre/app/crudAlunos.py
+++ Projeto Conclusao Semestre/app/crudAlunos.py
@@ -11,20 +11,20 @@ def listar_alunos():
         return jsonify({"error": "Failed to connect to the database"}), 500
     cursor = conn.cursor()
     try:
-        cursor.execute("SELECT * FROM alunos")
+        cursor.execute("SELECT * FROM Aluno")
         alunos = cursor.fetchall()
         return (
             jsonify(
                 [
                     {
-                        "aluno_id": aluno[0],
-                        "nome": aluno[1],
-                        "endereco": aluno[2],
-                        "cidade": aluno[3],
-                        "estado": aluno[4],
-                        "cep": aluno[5],
-                        "pais": aluno[6],
-                        "telefone": aluno[7],
+                        "id_aluno": aluno[0],
+                        "nome_completo": aluno[1],
+                        "data_nascimento": aluno[2],
+                        "id_turma": aluno[3],
+                        "nome_responsavel": aluno[4],
+                        "telefone_responsavel": aluno[5],
+                        "email_responsavel": aluno[6],
+                        "informacoes_adicionais": aluno[7],
                     }
                     for aluno in alunos
                 ]
@@ -48,18 +48,17 @@ def cadastrar_aluno():
     try:
         cursor.execute(
             """
-            INSERT INTO alunos (aluno_id, nome, endereco, cidade, estado, cep, pais, telefone)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO Aluno (nome_completo, data_nascimento, id_turma, nome_responsavel, telefone_responsavel, email_responsavel, informacoes_adicionais)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
             """,
             (
-                data["aluno_id"],
-                data["nome"],
-                data.get("endereco"),
-                data.get("cidade"),
-                data.get("estado"),
-                data.get("cep"),
-                data.get("pais"),
-                data.get("telefone"),
+                data["nome_completo"],
+                data["data_nascimento"],
+                data.get("id_turma"),
+                data["nome_responsavel"],
+                data["telefone_responsavel"],
+                data["email_responsavel"],
+                data.get("informacoes_adicionais"),
             ),
         )
         conn.commit()
@@ -72,8 +71,8 @@ def cadastrar_aluno():
         conn.close()
 
 
-@app.route("/alunos/<string:aluno_id>", methods=["PUT"])
-def alterar_aluno(aluno_id):
+@app.route("/alunos/<int:id_aluno>", methods=["PUT"])
+def alterar_aluno(id_aluno):
     data = request.get_json()
     conn = bd.create_connection()
     if conn is None:
@@ -82,19 +81,19 @@ def alterar_aluno(aluno_id):
     try:
         cursor.execute(
             """
-            UPDATE alunos
-            SET nome = %s, endereco = %s, cidade = %s, estado = %s, cep = %s, pais = %s, telefone = %s
-            WHERE aluno_id = %s
+            UPDATE Aluno
+            SET nome_completo = %s, data_nascimento = %s, id_turma = %s, nome_responsavel = %s, telefone_responsavel = %s, email_responsavel = %s, informacoes_adicionais = %s
+            WHERE id_aluno = %s
             """,
             (
-                data["nome"],
-                data.get("endereco"),
-                data.get("cidade"),
-                data.get("estado"),
-                data.get("cep"),
-                data.get("pais"),
-                data.get("telefone"),
-                aluno_id,
+                data["nome_completo"],
+                data["data_nascimento"],
+                data.get("id_turma"),
+                data["nome_responsavel"],
+                data["telefone_responsavel"],
+                data["email_responsavel"],
+                data.get("informacoes_adicionais"),
+                id_aluno,
             ),
         )
         conn.commit()
@@ -107,14 +106,14 @@ def alterar_aluno(aluno_id):
         conn.close()
 
 
-@app.route("/alunos/<string:aluno_id>", methods=["DELETE"])
-def excluir_aluno(aluno_id):
+@app.route("/alunos/<int:id_aluno>", methods=["DELETE"])
+def excluir_aluno(id_aluno):
     conn = bd.create_connection()
     if conn is None:
         return jsonify({"error": "Failed to connect to the database"}), 500
     cursor = conn.cursor()
     try:
-        cursor.execute("DELETE FROM alunos WHERE aluno_id = %s", (aluno_id,))
+        cursor.execute("DELETE FROM Aluno WHERE id_aluno = %s", (id_aluno,))
         conn.commit()
         return jsonify({"message": "Aluno excluído com sucesso"}), 200
     except Exception as e:
